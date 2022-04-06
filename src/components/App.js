6dd6733f517c33d00aa068dbea076f3f00301689
Marcin Pawlus <marcinpawlus97@gmail.com>
2022-04-06 13:51:44 +0200

    Add Welcome component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import Welcome from './Welcome';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <p>My presentation site - EARLY STATE - WORK IN PROGESS</p>
+      <Welcome/>
     </div>
   );
 }
