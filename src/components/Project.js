9fee9c813e760daddaa80439afbd88efd0535586
Marcin Pawlus <marcinpawlus97@gmail.com>
2022-04-29 13:36:44 +0200

    Add single project li display

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../styles/Project.css';
 
-const Project = () => {
+const Project = ({project}) => {
+
+    const {name, link, desc} = project;
+
     return ( 
         <>
+            <li className='liProject'>
+                <p className='oneMovieA'>
+                    <strong>{name}</strong>, Description: {desc}, (Link: {link})
+                </p>
+            </li>
         </>
      );
 }
